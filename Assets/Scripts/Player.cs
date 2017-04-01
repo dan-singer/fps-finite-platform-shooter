9e8ab56b4c1b7a1fed7c7a6dc6965ea10579e04e
Dan <dsinger.sax@gmail.com>
2017-04-01 04:35:59 +0000

    Test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,32 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+/// <summary>
+/// The main Player class
+/// </summary>
 class Player : MonoBehaviour
 {
+
+
+
     //Gravitational Acceleration
-    public Vector3 Gravity;
+    public float Gravity;
+    public float JumpYVelocity;
 
     //Speed to move and rotation
     public float MoveSpeed;
     public float RotationSpeed;
 
 
+    public float MaxRotationAngle, MinRotationAngle;
+
+
+
     private CharacterController controller;
     private Camera camera;
 
     //Actual Vector used for movement
-    private Vector3 movement;
+    private Vector3 velocity;
     private Vector3 rotation;
+    private Vector3 acceleration;
 
+    //Mouse stuff
+    private float rotationX = 0, rotationY = 0;
+    private Quaternion origRotation;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
+        acceleration = Vector3.zero;
+        origRotation = transform.localRotation;
+        velocity = Vector3.zero;
+        camera = transform.GetChild(0).GetComponent<Camera>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         //For translation
         float horzMove = Input.GetAxis("Horizontal");
@@ -38,18 +56,64 @@ class Player : MonoBehaviour
         float camX = Input.GetAxis("Mouse X");
         float camY = Input.GetAxis("Mouse Y");
 
+
         //Movement
-        movement = new Vector3(horzMove, 0, vertMove) * Time.deltaTime * MoveSpeed;
-        movement = transform.TransformDirection(movement);
+        Vector3 oldVel = velocity;
 
-        //Camera rotation
-        rotation = new Vector3(-camY, camX, 0) * Time.deltaTime * RotationSpeed;
+        velocity.x = horzMove;
+        velocity.z = vertMove;
+        velocity *= Time.deltaTime * MoveSpeed;
+        velocity = transform.TransformDirection(velocity);
 
-        controller.Move(movement);
-        transform.eulerAngles += rotation;
-        
-        
+        velocity = new Vector3(velocity.x, oldVel.y, velocity.z);
+
+
+        //Don't clamp X
+        rotationX += camX * RotationSpeed * Time.deltaTime;
+        Quaternion xQuat = Quaternion.AngleAxis(rotationX, Vector3.up);
+
+        //Do clamp Y
+        rotationY += camY * RotationSpeed * Time.deltaTime;
+        rotationY = ClampAngle(rotationY, MinRotationAngle, MaxRotationAngle);
+        Quaternion yQuat = Quaternion.AngleAxis(-rotationY, Vector3.right);
+
+        //Construct new rotation
+        transform.localRotation = origRotation * xQuat;
+        camera.transform.localRotation = origRotation * yQuat;
+
+        //Jumping
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = JumpYVelocity * Time.deltaTime;
+            }
+        }
+
+        //Gravitational Acceleration
+        velocity.y += Gravity * Time.deltaTime;
+
+        //Actually move and stuff
+        controller.Move(velocity);
+
     }
 
+    /// <summary>
+    /// Clamps an angle. Thanks to http://answers.unity3d.com/questions/29741/mouse-look-script.html
+    /// </summary>
+    /// <param name="angle"></param>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle <= -360F)
+         angle += 360F;
+        if (angle >= 360F)
+         angle -= 360F;
+
+
+        return Mathf.Clamp(angle, min, max);
+    }
 }
 
