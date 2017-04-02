df3165d07bec0f07a96dfb2c68911990aa25c4b6
Nick Marano <ngm9939@rit.edu>
2017-04-02 03:34:27 +0000

    thumbs

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+
+    private int score;
+
+    public Text scoreText;
+
+    public Text blockType;
+
+    Player p;
+
+	// Use this for initialization
+	void Start () {
+
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        blockType = GameObject.Find("BlockType").GetComponent<Text>();
+
+        p = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        scoreText.text = "Score: " + score;
+
+        blockType.text = "Block type: " + p.CurrentBlock;
+	}
+}
