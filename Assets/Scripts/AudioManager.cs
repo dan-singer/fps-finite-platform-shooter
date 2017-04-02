c943d8efdbc809ec2c246ffb30ecf8d40c81312a
Dan <dsinger.sax@gmail.com>
2017-04-02 14:47:29 +0000

    Created Main Menu

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+
+    private static AudioManager instance;
+    /// <summary>
+    /// Singleton design pattern
+    /// </summary>
+    public static AudioManager Instance
+    {
+        get
+        {
+            //If instance hasn't been assigned to anything 
+            if (instance == null)
+            {
+                //If we found too many, it's bad
+                if (FindObjectsOfType<AudioManager>().Length > 1)
+                {
+                    Debug.LogError("Too many singletons!");
+                    return instance;
+                }
+                //Assign instance to the AudioManager we found
+                instance = FindObjectOfType<AudioManager>();
+
+                //If we didn't find one, make it
+                if (instance == null)
+                {
+                    GameObject singleton = new GameObject();
+                    instance = singleton.AddComponent<AudioManager>();
+                    singleton.AddComponent<AudioSource>();
+                    singleton.name = "AudioManager (Singleton)";
+                    DontDestroyOnLoad(singleton);
+                }
+
+            }
+            return instance;
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves, rotates, or scales objects 
+/// </summary>
+public class Mover : MonoBehaviour {
+
+    public Vector3 Velocity;
+    public Vector3 RotationalVelocity;
+    public Vector3 MaxScale;
+
+    public float MinX, MaxX, MinY, MaxY;
+
+    public bool generateRandomValues = true;
+
+    private Vector3 startScale;
+	// Use this for initialization
+	void Start () {
+        startScale = transform.localScale;
+
+        if (generateRandomValues)
+        {
+            System.Random r = new System.Random();
+            float mult = 10;
+
+            Velocity = new Vector3((float)r.NextDouble() * mult, (float)r.NextDouble() * mult, 0);
+            RotationalVelocity = new Vector3((float)r.NextDouble() * mult, (float)r.NextDouble() * mult, (float)r.NextDouble() * mult);
+            MaxScale = new Vector3((float)r.NextDouble() * mult, (float)r.NextDouble() * mult, (float)r.NextDouble() * mult);
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Velocity * Time.deltaTime);
+        transform.Rotate(RotationalVelocity * Time.deltaTime);
+        transform.localScale = new Vector3(Mathf.PingPong(Time.time, MaxScale.x), Mathf.PingPong(Time.time, MaxScale.y), Mathf.PingPong(Time.time, MaxScale.z));
+
+        //Keep in view
+        Vector3 viewport = Camera.main.WorldToViewportPoint(transform.position);
+        if (viewport.x > 1 || viewport.x < 0)
+            Velocity.x *= -1;
+        if (viewport.y < 0 || viewport.y > 1)
+            Velocity.y *= -1;
+
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        //viewport.y = Mathf.Clamp01(viewport.y);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Velocity *= -1;
+    }
+}
